da097efef24d080978f359da13db0bb68ff19828
SkyezerFox <actuallyori@gmail.com>
2022-01-13 01:04:44 +0000

    chore: update proto upstream

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 //! # furink-proto
 //! Rust definitions for the fur.ink gRPC backend services.
 
-pub mod database {
-    tonic::include_proto!("furink.database");
+pub mod users {
+    tonic::include_proto!("furink.users");
+}
+
+pub mod posts {
+    tonic::include_proto!("furink.posts");
 }
 pub mod session {
     tonic::include_proto!("furink.session");
