4a74ffe8e014fdc296f5fe5815e80ee2c1b618d3
kaylendog <actuallyori@gmail.com>
2022-04-17 22:01:40 +0100

    build: update build script

--- build.rs
+++ build.rs
@@ -1,11 +1,15 @@
+use std::{ffi::OsString, fs::read_dir};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().compile(
-        &[
-            "proto/posts.proto",
-            "proto/session.proto",
-            "proto/users.proto",
-        ],
-        &["proto"],
-    )?;
+    let paths: Vec<OsString> = read_dir("proto")
+        .unwrap()
+        .map(|entry| entry.unwrap())
+        .filter(|entry| {
+            entry.file_type().unwrap().is_file()
+                && entry.file_name().to_str().unwrap().ends_with(".proto")
+        })
+        .map(|entry| entry.file_name())
+        .collect();
+    tonic_build::configure().compile(&paths, &["proto"])?;
     Ok(())
 }
