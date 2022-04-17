0cd69965c01a8a66e7b68f4ba6b16d0c0b46091b
kaylendog <actuallyori@gmail.com>
2022-04-17 22:03:46 +0100

    feat: add service discovery module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 //! # furink-proto
 //! Rust definitions for the fur.ink gRPC backend services.
 
+pub mod discovery {
+	//! DIscoery service definitions.
+	tonic::include_proto!("furink.discovery");
+}
+
 pub mod users {
     //! Users service definitions.
     tonic::include_proto!("furink.users");
