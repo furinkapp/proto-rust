82f9a1f43de81b9f8cf216425bebc72bc1e81552
kaylendog <actuallyori@gmail.com>
2022-04-20 19:27:43 +0100

    feat(version): provide basic service impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //! # furink-proto
 //! Rust definitions for the fur.ink gRPC backend services.
 
-
 //! Protocol version string, compiled from git revision and semver.
 pub static VERSION: &'static str = env!("VERGEN_GIT_SEMVER");
 
@@ -27,4 +26,28 @@ pub mod session {
 pub mod version {
     //! Version service definitions.
     tonic::include_proto!("furink.version");
+
+    use tonic::{Code, Request, Response, Status};
+
+    use self::version_service_server::VersionService;
+    use super::VERSION;
+
+    /// A basic version verification service implementation.
+    pub struct VersionServiceProvider;
+
+    #[tonic::async_trait]
+    impl VersionService for VersionServiceProvider {
+        async fn validate(
+            &self,
+            request: Request<VersionRequest>,
+        ) -> Result<Response<VersionResponse>, Status> {
+            if request.get_ref().version == VERSION {
+                Ok(Response::new(VersionResponse {
+                    version: VERSION.to_string(),
+                }))
+            } else {
+                Err(Status::new(Code::InvalidArgument, "Version mismatch"))
+            }
+        }
+    }
 }
